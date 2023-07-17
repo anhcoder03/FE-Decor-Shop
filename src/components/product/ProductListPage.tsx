5aa6f33a97aa4a3a90d9bb4c8027a2694ef70710
nguyenhongson <nguyenhongson20433@gmail.com>
2023-07-17 18:50:38 +0700

    fe/UI-Update-ProductPage

--- src/components/product/ProductListPage.tsx
+++ src/components/product/ProductListPage.tsx
@@ -4,17 +4,21 @@ import ProductListHeading from "./ProductListHeading";
 import ProductFilterPrice from "./ProductFilterPrice";
 import ProductCategoryList from "./ProductCategoryList";
 import ProductList from "./ProductList";
+import ProductDetailHeader from "../../modules/productDetail/ProductDetailHeader";
 
 const ProductListPage = () => {
   return (
-    <div className="container">
-      <ProductListHeading></ProductListHeading>
-      <div className="main grid grid-cols-custom pt-[30px] gap-30px pb-[30px]">
-        <div className="">
-          <ProductFilterPrice></ProductFilterPrice>
-          <ProductCategoryList></ProductCategoryList>
+    <div className="">
+      <ProductDetailHeader nameProduct=""></ProductDetailHeader>
+      <div className="container">
+        <ProductListHeading></ProductListHeading>
+        <div className="main grid grid-cols-custom gap-30px pb-[30px]">
+          <div className="">
+            <ProductFilterPrice></ProductFilterPrice>
+            <ProductCategoryList></ProductCategoryList>
+          </div>
+          <ProductList></ProductList>
         </div>
-        <ProductList></ProductList>
       </div>
     </div>
   );
