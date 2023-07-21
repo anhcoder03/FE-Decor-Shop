6af1651e4ad887f6e395c70ac4de15933fa4325b
anhcoder03 <nguyenphianh161003@gmail.com>
2023-07-21 22:41:36 +0700

    fe/loading homepage skeleton

--- src/components/homepage/TopRateProduct.tsx
+++ src/components/homepage/TopRateProduct.tsx
@@ -7,6 +7,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import { Tproduct } from "../../types/product";
 import { getAllProduct } from "../../api/product";
+import CardSkeleton from "../common/CardSkeleton";
 
 const TopRateProduct = () => {
   const [products, setProducts] = useState<Tproduct[]>([]);
@@ -23,36 +24,75 @@ const TopRateProduct = () => {
     <div>
       <Heading className="mb-10">ĐÈN TRANG TRÍ</Heading>
       <div className="product-swiper">
-        <Swiper
-          spaceBetween={10}
-          slidesPerView={2}
-          modules={[Navigation]}
-          loop={true}
-          navigation
-          breakpoints={{
-            1440: {
-              slidesPerView: 5,
-            },
-            970: {
-              slidesPerView: 4,
-            },
-            768: {
-              slidesPerView: 4,
-            },
-            720: {
-              slidesPerView: 3,
-            },
-            360: {
-              slidesPerView: 2,
-            },
-          }}
-        >
-          {products.map((item, index) => (
-            <SwiperSlide className="grid grid-cols-5 gap-5 mb-10" key={index}>
-              <ProductItem data={item} key={item._id}></ProductItem>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        {!products.length && (
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={2}
+            modules={[Navigation]}
+            loop={true}
+            navigation
+            breakpoints={{
+              1440: {
+                slidesPerView: 5,
+              },
+              970: {
+                slidesPerView: 4,
+              },
+              768: {
+                slidesPerView: 4,
+              },
+              720: {
+                slidesPerView: 3,
+              },
+              360: {
+                slidesPerView: 2,
+              },
+            }}
+          >
+            {Array(9)
+              .fill(0)
+              .map((item, index) => (
+                <SwiperSlide
+                  className="grid grid-cols-5 gap-5 mb-10"
+                  key={index}
+                >
+                  <CardSkeleton></CardSkeleton>
+                </SwiperSlide>
+              ))}
+          </Swiper>
+        )}
+        {products.length > 0 && (
+          <Swiper
+            spaceBetween={10}
+            slidesPerView={2}
+            modules={[Navigation]}
+            loop={true}
+            navigation
+            breakpoints={{
+              1440: {
+                slidesPerView: 5,
+              },
+              970: {
+                slidesPerView: 4,
+              },
+              768: {
+                slidesPerView: 4,
+              },
+              720: {
+                slidesPerView: 3,
+              },
+              360: {
+                slidesPerView: 2,
+              },
+            }}
+          >
+            {products?.map((item, index) => (
+              <SwiperSlide className="grid grid-cols-5 gap-5 mb-10" key={index}>
+                <ProductItem data={item} key={item._id}></ProductItem>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </div>
   );
