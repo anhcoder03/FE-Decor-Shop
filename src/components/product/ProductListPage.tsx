6312471d698d79a593540f1d8a3625366b4b9d00
anhcoder03 <nguyenphianh161003@gmail.com>
2023-07-22 23:27:21 +0700

    fix/layout

--- src/components/product/ProductListPage.tsx
+++ src/components/product/ProductListPage.tsx
@@ -16,6 +16,7 @@ import { getAllCategory } from "../../api/category";
 import { Tproduct } from "../../types/product";
 import { getAllProduct } from "../../api/product";
 import ReactPaginate from "react-paginate";
+import { Paginate } from "../paginate";
 
 const ProductListPage = () => {
   const [categories, setCategories] = useState<ICategory[]>([]);
@@ -98,27 +99,10 @@ const ProductListPage = () => {
           </div>
           <div>
             <ProductList loading={loading} products={products}></ProductList>
-            <div className="py-10">
-              <ReactPaginate
-                hrefBuilder={() => {
-                  return "#";
-                }}
-                breakLabel="..."
-                nextLabel=">"
-                onPageChange={handlePageClick}
-                pageCount={pageCount}
-                disableInitialCallback={true}
-                previousLabel="<"
-                renderOnZeroPageCount={null}
-                className="mb-10 flex flex-wrap items-center justify-center gap-x-2 gap-y-[6px] text-[15px] text-[#ececec] lg:gap-x-3 lg:text-base lg:mb-0 "
-                pageLinkClassName="bg-[#222222] bg-opacity-80 page-link transition-all hover:bg-opacity-100 py-1 px-2 rounded-[5px]"
-                previousClassName="bg-[#222222] bg-opacity-80  transition-all hover:bg-opacity-100 py-1 px-2 rounded-[5px]"
-                nextClassName="bg-[#222222] nextPage bg-opacity-80  transition-all hover:bg-opacity-100 py-1 px-2 rounded-[5px]"
-                activeClassName="page-active text-primary"
-                disabledClassName="opacity-40"
-                disabledLinkClassName="hover:cursor-default"
-              />
-            </div>
+            <Paginate
+              handlePageClick={handlePageClick}
+              pageCount={pageCount}
+            ></Paginate>
           </div>
         </div>
       </div>
